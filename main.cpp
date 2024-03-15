db1699d72b4621102e44e39eee9e067cffdcbe79
mar <144652524+saicity@users.noreply.github.com>
2024-03-15 15:57:32 -0400

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "Speedometer.h"
 #include "Transmission.h"
+#include "Sensor.h"
+#include "MassAirflowSensor.h"
 
 int main() {
-  Speedometer s1(30);
-  Transmission t1(1, false, false, true, "Automatic");
+  Speedometer s1(100);
+  Transmission t1(4, false, false, true, "Automatic");
+  MassAirflowSensor MAS1(true, true, s1.getSpeed(), s1);
+
   std::cout << s1;
   std::cout << t1;
   t1.reccomendGear();
   std::cout << t1;
+  std::cout << MAS1;
 }
+
